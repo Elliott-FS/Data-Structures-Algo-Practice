ef2f2e60af8494fc4ef315b00f547fc81124f8f6
Elliott Delano Wynn III <delanowynn@yahoo.com>
2023-04-24 19:47:01 -0400

    OOP class method and starting linked lists

--- DSPractice/DataStructures/basicClassSyntax.js
+++ DSPractice/DataStructures/basicClassSyntax.js
@@ -3,8 +3,23 @@ class Student {
         this.firstName = firstName;
         this.lastName = lastName; 
         this.grade = year;
+        this.tardies = 0;
     }
 
+    static enrollStudents(...Students){
+        // maybe email here
+        // static used to add utility functions
+        // that come as a default meathod on 
+        // all intances of class
+        // you would call this by saying Student.enroll student rather 
+        // than firstStudent.enrolStudents
+    }
+
+
+    markLate(){
+        this.tardies += 1;
+        return `Wow ${this.firstName} ${this.lastName} has been late ${this.tardies} times`;
+    }
     fullName() {
         return `Your full name is ${this.firstName} ${this.lastName}`;
 }
---
+++ DSPractice/DataStructures/singlyLinkedLists.js
@@ -0,0 +1,7 @@
+// A linked list is a list of data. It contains a train car structure with
+// head, tail, and length attributes. There are no indices, instead each element is 
+// called a node. 
+
+// A linked list consist of nodes each node has a value and a pointer to the next node.
+// Random access not allowed. 
+
